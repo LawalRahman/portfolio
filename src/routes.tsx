b5af17edc2b7564bf2262b322e89dd5566dd81f9
Rahman Lawal <rahmanabimbina@gmail.com>
2023-09-03 11:23:10 +0100

    add pofrtfolio to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from "./pages";
 
-const router = createBrowserRouter([{ path: "/", element: <Index /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Index /> },
+  { path: "/portfolio", element: <Index /> },
+]);
 
 export default router;
